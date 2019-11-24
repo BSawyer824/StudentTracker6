7e664ea758f28770f0f7094a871e18a9b2e693b7
BSawyer824 <40338647+BSawyer824@users.noreply.github.com>
2019-11-24 07:56:12 -0800

    Create shell for Course Detail View Model

---
+++ app/src/main/java/com/wgu_android/studenttracker6/ViewModels/CourseDetailViewModel.java
@@ -0,0 +1,12 @@
+package com.wgu_android.studenttracker6.ViewModels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class CourseDetailViewModel extends AndroidViewModel {
+    public CourseDetailViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
