62211e22d6f021897f27d4d92889e75e863f15fa
BSawyer824 <40338647+BSawyer824@users.noreply.github.com>
2019-11-29 09:04:14 -0800

    add date picker to Assessment Activity

--- app/src/main/java/com/wgu_android/studenttracker6/AssessmentDetailActivity.java
+++ app/src/main/java/com/wgu_android/studenttracker6/AssessmentDetailActivity.java
@@ -1,9 +1,11 @@
 package com.wgu_android.studenttracker6;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.wgu_android.studenttracker6.Entities.AssessmentEntity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,9 +13,39 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AssessmentDetailActivity extends AppCompatActivity {
 
+
+    //**************************************************
+    //Assessment Variables
+    @BindView(R.id.spinnerAssessType)
+    Spinner spinner;
+
+    @BindView(R.id.editTextAssessmentName)
+    EditText mEditTextAssessmentName;
+
+    @BindView(R.id.editTextGoalDate)
+    EditText mEditTextGoalDate;
+
+    @BindView(R.id.editTextDueDate)
+    EditText mEditTextDueDate;
+
+    final Calendar myCalendarGoal = Calendar.getInstance();
+    final Calendar myCalendarDue = Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener goalDate;
+    DatePickerDialog.OnDateSetListener dueDate;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +57,67 @@ public class AssessmentDetailActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+        ButterKnife.bind(this);
+        initSpinner();
+        initRecyclerView();
+        initViewModel();
+
+
+
+        //*****************************************************
+        //Make Goal and Due Date fields have a calendar date picker
+
+        //Goal Date - Date Picker
+        goalDate = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendarGoal.set(Calendar.YEAR, year);
+                myCalendarGoal.set(Calendar.MONTH, monthOfYear);
+                myCalendarGoal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabelGoal();
+            }
+        };
+
+        mEditTextGoalDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(AssessmentDetailActivity.this, goalDate, myCalendarGoal
+                        .get(Calendar.YEAR), myCalendarGoal.get(Calendar.MONTH),
+                        myCalendarGoal.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        //Due Date - Date Picker
+        dueDate = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendarDue.set(Calendar.YEAR, year);
+                myCalendarDue.set(Calendar.MONTH, monthOfYear);
+                myCalendarDue.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabelDue();
+            }
+        };
+
+        mEditTextDueDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(AssessmentDetailActivity.this, dueDate, myCalendarDue
+                        .get(Calendar.YEAR), myCalendarDue.get(Calendar.MONTH),
+                        myCalendarDue.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+        
+    }
+
+    //*****************************************************************************************
+    //recycler view methods    
+    
+    private void initViewModel() {
+    }
+
+    private void initRecyclerView() {
     }
 
 
@@ -62,12 +155,47 @@ public class AssessmentDetailActivity extends AppCompatActivity {
 
 //        spinnerSelectedItem = spinner.getSelectedItem().toString();
 //
-//        mViewModel.saveCourse(mEditTextCourseName.getText().toString(), myCalendarStart.getTime(),
-//                myCalendarEnd.getTime(), mEditTextMentor.getText().toString(), mEditTextPhone.getText().toString(),
+//        mViewModel.saveCourse(mEditTextCourseName.getText().toString(), myCalendarGoal.getTime(),
+//                myCalendarDue.getTime(), mEditTextMentor.getText().toString(), mEditTextPhone.getText().toString(),
 //                mEditTextEmail.getText().toString(), mEditTextNotes.getText().toString(), spinnerSelectedItem);
 
         finish();
     }
 
+    //**********************************************************************
+    //Spinner Methods
+    private void initSpinner() {
+    }
 
+
+    //**************************************************************************************
+    //Date Picker Methods
+    private void updateLabelGoal() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        mEditTextGoalDate.setText(sdf.format(myCalendarGoal.getTime()));
+    }
+
+    private void setLabelGoal(AssessmentEntity assessmentEntity) {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        mEditTextGoalDate.setText(sdf.format(assessmentEntity.getAssessmentGoalDate()));
+    }
+
+    private void updateLabelDue() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        mEditTextDueDate.setText(sdf.format(myCalendarDue.getTime()));
+    }
+
+    private void setLabelDue(AssessmentEntity assessmentEntity) {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        mEditTextDueDate.setText(sdf.format(assessmentEntity.getAssessmentDueDate()));
+    }
+    
 }
